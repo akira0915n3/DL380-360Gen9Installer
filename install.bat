abe72297d06c47ad90c54791f2090f492278c4a4
akira0915n3 <akira0915n3@outlook.com>
2022-11-22 15:57:51 +0900

    rdp >> ps1

--- install.bat
+++ install.bat
@@ -45,11 +45,7 @@ PowerShell -command "Install-WindowsFeature -Name SNMP-WMI-Provider -IncludeMana
 echo "役割と機能のインストールが完了しました。"
 
 :rdp-arrow
-rem -リモートデスクトップの有効化
-PowerShell -command Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type Dword -Value "0"
-PowerShell -command Set-NetFirewallRule -DisplayName "リモート デスクトップ - シャドウ (TCP 受信)" -Enabled True
-PowerShell -command Set-NetFirewallRule -DisplayName "リモート デスクトップ - ユーザー モード (TCP 受信)" -Enabled True
-PowerShell -command Set-NetFirewallRule -DisplayName "リモート デスクトップ - ユーザー モード (UDP 受信)" -Enabled True
+rdp.ps1
 
 
 set errorlevel = 0
